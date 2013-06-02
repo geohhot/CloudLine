af5cd2c37d7b953591fe199da3ebe02f142bf912
geohhot <ghinoyan@gmail.com>
2013-06-02 11:57:40 +0400

    Added error message in case that city not found

--- cloudline.py
+++ cloudline.py
@@ -37,26 +37,32 @@ location = urllib2.quote (location)
 
 url = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+location+"&mode=json&units=metric&cnt="+str(days)
 
+print url
+
 response = requests.get (url)
 
 if response.status_code == 200:
 	weather = response.json()
-	days = weather['list']
-	for day in days:
-		date = time.strftime ("%B %d, %a %Y", time.localtime (int(day['dt'])))
-		main = day['weather'][0]['main']
-		description = day['weather'][0]['description']
-		temp_min = day['temp']['min']
-		temp_max = day['temp']['max']
-		pressure = day['pressure']
-		humidity = day['humidity']
-		print date
-		print ("--%s (%s)") % (main, description)
-		print ("--Temperature")
-		print ("----min: %s") % (temp_min)
-		print ("----max: %s") % (temp_max)
-		print ("--Pressure: %s") % (pressure)
-		print ("--Humidity: %s") % (humidity)
-
+	code = weather['cod']
+	if code == "404":
+		print "Sorry, location not found."
+		sys.exit (0)
+	elif code == "200":
+		days = weather['list']
+		for day in days:
+			date = time.strftime ("%B %d, %a %Y", time.localtime (int(day['dt'])))
+			main = day['weather'][0]['main']
+			description = day['weather'][0]['description']
+			temp_min = day['temp']['min']
+			temp_max = day['temp']['max']
+			pressure = day['pressure']
+			humidity = day['humidity']
+			print date
+			print ("--%s (%s)") % (main, description)
+			print ("--Temperature")
+			print ("----min: %s") % (temp_min)
+			print ("----max: %s") % (temp_max)
+			print ("--Pressure: %s") % (pressure)
+			print ("--Humidity: %s") % (humidity)
 else:
 	print "Err. Can't load resource. :("
